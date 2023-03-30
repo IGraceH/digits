da5eaf4ede2772fa2d22142a2ed2b1198ab36da5
Isabelle <87943193+IGraceH@users.noreply.github.com>
2023-03-29 19:27:17 -1000

    contacts collection fix

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,27 +1,32 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
-import { Contacts } from '../../api/stuff/Contacts.js';
+import { Contacts } from '../../api/contact/Contacts.js';
 
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
-const addData = (data) => {
+const addStuffData = (data) => {
   console.log(`  Adding: ${data.name} (${data.owner})`);
   Stuffs.collection.insert(data);
+};
+
+const addContactsData = (data) => {
+  console.log(`  Adding: ${data.firstName} ${data.lastName} (${data.owner})`);
+  Contacts.collection.insert(data);
 };
 
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating stuffs default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
+    Meteor.settings.defaultData.forEach(data => addStuffData(data));
   }
 }
 
 // Initialize the ContactsCollection if empty.
 if (Contacts.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
+  if (Meteor.settings.defaultContacts) {
     console.log('Creating contacts default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
+    Meteor.settings.defaultContacts.forEach(data => addContactsData(data));
   }
 }
